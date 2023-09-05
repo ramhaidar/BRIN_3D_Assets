6ff2c94654b7c83d240ba55f454b2f44e1f56b58
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2023-09-05 20:50:57 +0700

    0.2.2
    Added Captcha (Mews\Captcha) mechanism and implement it to Registration Page.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -77,6 +77,8 @@
     </style>
 
     @yield('customStyle')
+
+    @yield('topScript')
 </head>
 
 <body>
@@ -135,6 +137,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js"
         integrity="sha512-3dZ9wIrMMij8rOH7X3kLfXAzwtcHpuYpEgQg1OA4QAob1e81H8ntUQmQm3pBudqIoySO5j0tHN4ENzA6+n2r4w=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    @yield('bottomScript')
 </body>
 
 </html>
